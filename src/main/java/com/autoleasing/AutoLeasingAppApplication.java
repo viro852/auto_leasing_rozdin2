ebb35a0038d447d8c0635598522260fd156ae15e
Denis Burda <Denis.Burda@netent.com>
2021-07-04 17:01:56 +0300

    init commit

---
+++ src/main/java/com/autoleasing/AutoLeasingAppApplication.java
@@ -0,0 +1,13 @@
+package com.autoleasing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLeasingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoLeasingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoleasing/AutoLeasingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoleasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoLeasingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
